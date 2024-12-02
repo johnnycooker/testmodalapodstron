61b94929d07f6c5eae34af604162a45cdb56d287
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-02 15:24:47 +0100

    Update src/pages/testing/alleluja/index.tsx

---
+++ src/pages/testing/alleluja/index.tsx
@@ -0,0 +1,15 @@
+
+      import React from 'react';
+  
+      const GeneratedPage: React.FC = () => {
+        return (
+          <div>
+            <section className="h-[50vh]" style={{ backgroundColor: "#2f7475" }}>
+          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(18.854166666666686vw) translateY(0vh)", width: "62.1875vw", height: "50.0544069640914vh", position: "absolute" }} />
+        </section>
+          </div>
+        );
+      };
+  
+      export default GeneratedPage;
+    
