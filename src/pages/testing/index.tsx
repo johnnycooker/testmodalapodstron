82e4e1832754e8891ea639723eb9241694b08fd7
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-02 15:20:10 +0100

    Update src/pages/testing/index.tsx

---
+++ src/pages/testing/index.tsx
@@ -0,0 +1,15 @@
+
+      import React from 'react';
+  
+      const GeneratedPage: React.FC = () => {
+        return (
+          <div>
+            <section className="h-[50vh]" style={{ backgroundColor: "#458759" }}>
+          
+        </section>
+          </div>
+        );
+      };
+  
+      export default GeneratedPage;
+    
