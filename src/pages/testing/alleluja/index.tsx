51c1fc7d1f99de655373c5b1d462974f2edaf1f0
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-05 13:13:03 +0100

    Update src/pages/testing/alleluja/index.tsx

--- src/pages/testing/alleluja/index.tsx
+++ src/pages/testing/alleluja/index.tsx
@@ -4,8 +4,11 @@
       const GeneratedPage: React.FC = () => {
         return (
           <div>
-            <section className="h-[50vh]" style={{ backgroundColor: "#2f7475" }}>
-          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(18.854166666666686vw) translateY(0vh)", width: "62.1875vw", height: "50.0544069640914vh", position: "absolute" }} />
+            <section className="h-[100.19999999999999vh]" style={{ backgroundColor: "#2b5d53" }}>
+          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(0vw) translateY(0vh)", width: "19.53125vw", height: "37.13084874863981vh", position: "absolute" }} />
+        </section>
+<section className="h-[103.5vh]" style={{ backgroundColor: "#f0f0f0" }}>
+          
         </section>
           </div>
         );
