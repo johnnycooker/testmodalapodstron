bbf766c2dbf71d76976b1cebc56158be293d1083
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-06 12:01:04 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,13 +4,7 @@
       const GeneratedPage: React.FC = () => {
         return (
           <div>
-            <section className="h-[50vh]" style={{ backgroundColor: "#742525" }}>
-          
-        </section>
-<section className="h-[209.5vh]" style={{ backgroundColor: "#f0f0f0" }}>
-          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(0vw) translateY(0vh)", width: "100vw", height: "76.9375vh", position: "absolute" }} />
-<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(0vw) translateY(0vh)", width: "18.84375vw", height: "12.5625vh", position: "absolute" }} />
-        </section>
+            <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(0vw) translateY(0vh)", width: "100vw", height: "92.0625vh", position: "absolute" }} />
           </div>
         );
       };
