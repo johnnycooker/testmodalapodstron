6b5f9d628e935440bbdfaf7e849436118056ba68
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-06 20:39:53 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@
       const GeneratedPage: React.FC = () => {
         return (
           <div>
-            <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(0vw) translateY(0vh)", width: "100vw", height: "100.0625vh", position: "absolute" }} />
+            <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(0vw) translateY(0vh)", width: "100vw", height: "50.1875vh", position: "absolute" }} />
           </div>
         );
       };
