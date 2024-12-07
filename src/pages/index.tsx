b92bd85ebb504bc03c4487de56ca0a46061fc685
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-07 23:22:46 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,23 @@
       const GeneratedPage: React.FC = () => {
         return (
           <div>
-            <section className="h-[78.5vh]" style={{ backgroundColor: "#a41919" }}>
+            <section className="h-[78.5vh]" style={{ position: "relative", backgroundColor: "#a41919" }}>
           <div className="" style={{ backgroundColor: "#289f2a", width: "83.85416666666667%", height: "81.41470180305132%", left: "1.5104166666666674%", top: "4.160887656033294%", position: "absolute" }}>
           <div className="" style={{ backgroundColor: "#f0f0f0", width: "59.006211180124225%", height: "80.23850085178876%", left: "38.69565217391308%", top: "13.798977853492335%", position: "absolute" }}>
           <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "5.05263157894737%", top: "9.55414012738854%", width: "85.05263157894737%", height: "71.44373673036094%", position: "absolute" }} />
         </div>
         </div>
+        </section>
+<section className="h-[88.1vh]" style={{ position: "relative", backgroundColor: "#309097" }}>
+          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "6.197916666666666%", top: "8.271604938271606%", width: "30.52083333333333%", height: "28.641975308641975%", position: "absolute" }} />
+<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "38.54166666666667%", top: "20.00000000000001%", width: "26.19791666666667%", height: "25.061728395061728%", position: "absolute" }} />
+<div className="" style={{ backgroundColor: "#248970", width: "17.8125%", height: "63.95061728395061%", left: "73.43749999999997%", top: "20.86419753086417%", position: "absolute" }}>
+          
+        </div>
+<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "28.645833333333346%", top: "49.382716049382765%", width: "40.260416666666664%", height: "39.01234567901235%", position: "absolute" }} />
+        </section>
+<section className="h-[50vh]" style={{ position: "relative", backgroundColor: "#f0f0f0" }}>
+          
         </section>
           </div>
         );
