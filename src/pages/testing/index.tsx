dae959e78f7227b9f201ec74db770fcfea1d6ff5
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-07 23:27:12 +0100

    Update src/pages/testing/index.tsx

--- src/pages/testing/index.tsx
+++ src/pages/testing/index.tsx
@@ -4,8 +4,15 @@
       const GeneratedPage: React.FC = () => {
         return (
           <div>
-            <section className="h-[50vh]" style={{ backgroundColor: "#458759" }}>
+            <section className="h-[50vh]" style={{ position: "relative", backgroundColor: "#458759" }}>
+          <div className="" style={{ backgroundColor: "#7b1919", width: "92.32558139534883%", height: "42.39130434782608%", left: "3.9534883720930236%", top: "50.65217391304347%", position: "absolute" }}>
           
+        </div>
+<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "3.72093023255814%", top: "3.0434782608695654%", width: "93.48837209302326%", height: "43.47826086956522%", position: "absolute" }} />
+        </section>
+<section className="h-[66.80000000000001vh]" style={{ position: "relative", backgroundColor: "#92de35" }}>
+          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "9.534883720930237%", top: "10.097719869706832%", width: "55.81395348837209%", height: "25.407166123778502%", position: "absolute" }} />
+<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "39.767441860465034%", top: "41.36807817589574%", width: "56.74418604651163%", height: "25.2442996742671%", position: "absolute" }} />
         </section>
           </div>
         );
