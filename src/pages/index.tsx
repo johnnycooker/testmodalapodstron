f674b78152635a8781ff7f5d9fc18df578a0039b
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-07 12:38:20 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,9 +5,7 @@
         return (
           <div>
             <section className="h-[78.5vh]" style={{ backgroundColor: "#a41919" }}>
-          <div className="" style={{ backgroundColor: "#f0f0f0", width: "67.8125vw", height: "62.704576976421635vh", transform: "translate(0, 0)", position: "absolute" }}>
-          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(0vw) translateY(0vh)", width: "96.08294930875577vw", height: "96.68141592920354vh", position: "absolute" }} />
-        </div>
+          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(0vw) translateY(0vh)", width: "84.89583333333333vw", height: "77.87794729542303vh", position: "absolute" }} />
         </section>
           </div>
         );
