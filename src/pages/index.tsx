3a5f5ea2a38a5882f263ab7b82a073809785fefe
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-07 23:17:51 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,11 @@
         return (
           <div>
             <section className="h-[78.5vh]" style={{ backgroundColor: "#a41919" }}>
-          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ transform: "translateX(0vw) translateY(0vh)", width: "84.89583333333333vw", height: "77.87794729542303vh", position: "absolute" }} />
+          <div className="" style={{ backgroundColor: "#289f2a", width: "83.85416666666667%", height: "81.41470180305132%", left: "1.5104166666666674%", top: "4.160887656033294%", position: "absolute" }}>
+          <div className="" style={{ backgroundColor: "#f0f0f0", width: "59.006211180124225%", height: "80.23850085178876%", left: "38.69565217391308%", top: "13.798977853492335%", position: "absolute" }}>
+          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "5.05263157894737%", top: "9.55414012738854%", width: "85.05263157894737%", height: "71.44373673036094%", position: "absolute" }} />
+        </div>
+        </div>
         </section>
           </div>
         );
