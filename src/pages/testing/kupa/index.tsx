70b854e958b81d8bdb9a6c97b20980b7644a8f9f
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-09 09:19:17 +0100

    Update src/pages/testing/kupa/index.tsx

---
+++ src/pages/testing/kupa/index.tsx
@@ -0,0 +1,27 @@
+
+      import React from 'react';
+  
+      const GeneratedPage: React.FC = () => {
+        return (
+          <div>
+            <div className="" style={{ backgroundColor: "#f0f0f0", width: "20%", height: "20%", left: "0%", top: "0%", position: "absolute" }}>
+          
+        </div>
+<section className="h-[83vh] md:h-[71.7vh] xl:h-[56.3vh]" style={{ position: "relative", backgroundColor: "#37764a" }}>
+          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "10.625000000000009%", top: "6.1994609164420496%", width: "44.322916666666664%", height: "48.517520215633425%", position: "absolute" }} />
+<div className="" style={{ backgroundColor: "#f0f0f0", width: "38.33333333333333%", height: "48.167115902964966%", left: "56.82291666666666%", top: "6.469002695417785%", position: "absolute" }}>
+          
+        </div>
+<div className="" style={{ backgroundColor: "#f0f0f0", width: "84.79166666666667%", height: "24.851752021563343%", left: "10.468749999999998%", top: "59.16442048517518%", position: "absolute" }}>
+          
+        </div>
+        </section>
+<section className="h-[73.9vh]" style={{ position: "relative", backgroundColor: "#58bbb9" }}>
+          
+        </section>
+          </div>
+        );
+      };
+  
+      export default GeneratedPage;
+    
