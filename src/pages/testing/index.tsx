f9f95fda511c1e98049590db60942cc70f69b286
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-18 19:58:28 +0100

    Update src/pages/testing/index.tsx

--- src/pages/testing/index.tsx
+++ src/pages/testing/index.tsx
@@ -1,18 +1,21 @@
 
       import React from 'react';
-  
+      import Link from 'next/link';
+
       const GeneratedPage: React.FC = () => {
         return (
           <div>
             <section className="h-[50vh]" style={{ position: "relative", backgroundColor: "#458759" }}>
-          <div className="" style={{ backgroundColor: "#7b1919", width: "92.32558139534883%", height: "42.39130434782608%", left: "3.9534883720930236%", top: "50.65217391304347%", position: "absolute" }}>
-          
+          <div className="w-[100%] h-[100%] left-[0%] top-[0%]" style={{ position: "absolute", backgroundColor: "#7b1919" }}>
+          <nav className="flex items-center justify-between p-4 h-[34.73913043478261%]" style={{ backgroundColor: "#333333" }}>
+          <div className="flex gap-4 items-center"><img src="https://9.allegroimg.com/s1024/0cbde8/0f4e067c4432bfe3642da1782279" alt="logo" className="w-[200px] h-[100px] ml:[5%] object-fill" /></div>
+<div className="flex gap-4 items-center"><Link href="/">main</Link> <Link href="/cmiczek">cmiczek</Link> <Link href="/testing">testing</Link> <Link href="/sprawdzenie">sprawdzenie</Link></div>
+        </nav>
         </div>
-<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "3.72093023255814%", top: "3.0434782608695654%", width: "93.48837209302326%", height: "43.47826086956522%", position: "absolute" }} />
         </section>
-<section className="h-[66.80000000000001vh]" style={{ position: "relative", backgroundColor: "#92de35" }}>
-          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "9.534883720930237%", top: "10.097719869706832%", width: "55.81395348837209%", height: "25.407166123778502%", position: "absolute" }} />
-<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="" style={{ left: "39.767441860465034%", top: "41.36807817589574%", width: "56.74418604651163%", height: "25.2442996742671%", position: "absolute" }} />
+<section className="h-[50vh]" style={{ position: "relative", backgroundColor: "#92de35" }}>
+          <img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[100%] h-[50%] left-[0%] top-[0%]" style={{ position: "absolute" }} />
+<img src="https://czat.ai/img/avatars/full-og/kotek.jpg" alt="" className="w-[100%] h-[50%] left-[0%] top-[0%]" style={{ position: "absolute" }} />
         </section>
           </div>
         );
