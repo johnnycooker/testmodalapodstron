0a80fde9f9d6dc5c455842fafb9314747dbfa55b
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-18 19:58:06 +0100

    Update src/pages/cmiczek/index.tsx

---
+++ src/pages/cmiczek/index.tsx
@@ -0,0 +1,17 @@
+
+      import React from 'react';
+      import Link from 'next/link';
+
+      const GeneratedPage: React.FC = () => {
+        return (
+          <div>
+            <nav className="flex items-center justify-between p-4 h-[11.703703703703702%]" style={{ backgroundColor: "#333333" }}>
+          <div className="flex gap-4 items-center"><img src="https://9.allegroimg.com/s1024/0cbde8/0f4e067c4432bfe3642da1782279" alt="logo" className="w-[200px] h-[100px] ml:[5%] object-fill" /></div>
+<div className="flex gap-4 items-center"><Link href="/">main</Link> <Link href="/cmiczek">cmiczek</Link> <Link href="/testing">testing</Link> <Link href="/sprawdzenie">sprawdzenie</Link></div>
+        </nav>
+          </div>
+        );
+      };
+  
+      export default GeneratedPage;
+    
