477d42d17c509a305eb73d57d5fd26f52a4f5667
johnnycooker <127840081+johnnycooker@users.noreply.github.com>
2024-12-19 10:11:04 +0100

    Update src/pages/index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,10 @@
         return (
           <div>
             <section className="h-[103vh] md:h-[113vh] xl:h-[103vh]" style={{ position: "relative", backgroundColor: "#641616" }}>
-          <img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yuumi_0.jpg" alt="" className="w-[90.03028100775195%] md:w-[48.70299796747969%] xl:w-[37.23958333333334%] h-[28.77507919746568%] md:h-[43.66420274551214%] left-[4.407703488372091%] md:left-[2.8620426829268295%] xl:left-[8.593750000000002%] top-[17.74023231256604%] md:top-[21.04984201199209%] xl:top-[37.90918690601903%]" style={{ position: "absolute" }} />
+          <img src="https://polszczyzna.pl/wp-content/uploads/2021/04/essa-co-to-znaczy-co-to-jest-slang-miejski-slownik-potoczny-jezyk-czym-jest-essa-synonim-super-ekstra-slownik-PolszczyznaPL.png" alt="" className="w-[90.03028100775195%] md:w-[48.70299796747969%] xl:w-[37.23958333333334%] h-[28.77507919746568%] md:h-[43.66420274551214%] left-[4.407703488372091%] md:left-[2.8620426829268295%] xl:left-[8.697916666666666%] top-[17.74023231256604%] md:top-[21.04984201199209%] xl:top-[38.225976768743415%]" style={{ position: "absolute" }} />
 <nav className="flex items-center justify-between p-4 h-[16.31168831168831%]" style={{ backgroundColor: "#1b92c5" }}>
           <div className="flex gap-4 items-center"><Link href="/">main</Link> <Link href="/cmiczek">cmiczek</Link></div>
-<div className="flex gap-4 items-center"><img src="https://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_0.jpg" alt="logo" className="w-[100px] md:w-[300px] h-[50px] md:h-[100px] ml:[5%] object-fill" /></div>
+<div className="flex gap-4 items-center"><img src="https://24jgora.pl/static/files/gallery/146/1381823_1670412077.jpg" alt="logo" className="w-[100px] md:w-[300px] h-[50px] md:h-[100px] ml:[5%] object-fill" /></div>
 <div className="flex gap-4 items-center"><Link href="/testing">testing</Link> <Link href="/sprawdzenie">sprawdzenie</Link></div>
         </nav>
 <div className="w-[89.19452519379848%] md:w-[40.46875%] h-[48.15205913410771%] md:h-[44.13938753959874%] left-[4.991521317829419%] md:left-[53.37779471544717%] xl:left-[50.572916666666636%] top-[47.62407602956698%] md:top-[20.838648770175745%] xl:top-[37.69799366420271%]" style={{ position: "absolute", backgroundColor: "#811ca6" }}>
